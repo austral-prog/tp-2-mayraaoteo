615454b68c28383aee9a7af3306992024afa0dde
mayraaoteo <162721507+mayraaoteo@users.noreply.github.com>
2024-03-22 10:55:43 -0300

    Intento 2 ada.py

--- ada.py
+++ ada.py
@@ -2,6 +2,6 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
 
-    print(f"{first_name}.title() {last_name}.title()")
-    print(f"{first_name}.upper() {last_name}.upper()")
-    print(f"	{first_name}.lower() {last_name}.lower()")
+    print(f"{first_name}.title()" + " " + f"{last_name}.title()")
+    print(f"{first_name}.upper()" + " " + f"{last_name}.upper()")
+    print(f"	{first_name}.lower()" + " " + f"{last_name}.lower()")
